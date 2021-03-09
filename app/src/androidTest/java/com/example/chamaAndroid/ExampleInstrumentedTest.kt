1f6a80cd71ffe6907b2b3e93f701dc85a829d6cd
jorgealbertojas <jorgealbertojas@gmail.com>
2021-03-09 12:50:39 -0300

    Put Google Places Api

--- app/src/androidTest/java/com/example/chamaAndroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/chamaAndroid/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.chamaAndroid
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/main/java/com/example/chamaAndroid/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.chamaAndroid
+
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var TAG = "MainActivity"
+
+    var placesClient: PlacesClient? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Initialize the AutocompleteSupportFragment.
+
+        val apiKey = getString(R.string.api_key)
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, apiKey)
+        }
+
+        // Create a new Places client instance.
+        placesClient = Places.createClient(this)
+
+
+        val autocompleteFragment =
+                supportFragmentManager.findFragmentById(R.id.place_autocomplete_fragment)
+                        as AutocompleteSupportFragment
+
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(p0: Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $p0")
+            }
+        })
+
+    }
+}
